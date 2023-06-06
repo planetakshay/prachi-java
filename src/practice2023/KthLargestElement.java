506b4bbfe81a3f8921466905f507891017f71b53
prachideodhar <prachi.deodhar@gmail.com>
2023-06-06 16:19:35 -0700

    practice

--- src/practice2023/KthLargestElement.java
+++ src/practice2023/KthLargestElement.java
@@ -3,6 +3,9 @@ package practice2023;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+/**
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/
+ */
 public class KthLargestElement {
 
     public static void main(String[] args) {
@@ -16,13 +19,18 @@ public class KthLargestElement {
     }
 
     public static int findKthLargestElement(int[] nums, int k) {
-        Queue kthLargestQueue = new PriorityQueue(k + 1);
+        // Here priority queue kthLargestQueue will hold elements in
+        // their natural sorted order up to k elements.
+        // The following code will discard the larger elements than
+        // Kth largest and at the end of the program the top most
+        // element in the queue will be kth largest.
+        Queue<Integer> kthLargestQueue = new PriorityQueue(k + 1);
         for (int i = 0; i < nums.length; i++) {
             kthLargestQueue.add(nums[i]);
             if (kthLargestQueue.size() > k) {
                 kthLargestQueue.poll();
             }
         }
-        return (int) kthLargestQueue.poll();
+        return kthLargestQueue.poll();
     }
 }
