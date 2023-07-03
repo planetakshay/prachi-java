e3fb5fd72546d5c717c71de126d1fd91fd3996f7
prachideodhar <prachi.deodhar@gmail.com>
2023-07-03 16:10:55 -0700

    new algorithm

--- src/practice/Matrix.java
+++ src/practice/Matrix.java
@@ -13,44 +13,28 @@ import java.util.Set;
 public class Matrix {
 
 	public static void main(String args[]) {
-
 		int array[][] = new int[][] { { 19, 3, 14 }, { 6, 28, 8 }, { 8, 7, 3 } };
-
 		Matrix matrix = new Matrix();
-
 		System.out.println("Main Diagonal\n");
-
 		matrix.printMainDiagonal(array);
-
 		System.out.println("\nSecond Diagonal\n");
-
 		matrix.printSecondDiagonal(array);
-
 		int array1[][] = new int[][] { { 10, 50, 80, 100 },
 				{ 60, 70, 110, 130 }, { 85, 100, 120, 140 },
 				{ 90, 200, 210, 300 } };
-
 		System.out.println("\nElement 230 is found: "
 				+ matrix.findElement(array1, 210, 4, 4));
-
 		int array2[][] = new int[][] { { 210, 150, 480, 10000 },
 				{ 960, 730, 10, 130 }, { 850, 100, 120, 140 },
 				{ 900, 200, 210, 3000 } };
-
 		System.out.println("Max Element in Array2: " + matrix.findMax(array2));
-
 		matrix.printSpiralOrder(array);
-
 		System.out.println("----------------------\n");
-
 		int[][] reset = new int[][] { { 1, 2, 3, 4 }, { 0, 5, 6, 7 },
 				{ 8, 0, 9, 10 } };
 		matrix.printMatrix(reset);
-
 		reset = matrix.resetValues(reset);
-
 		System.out.println("Now printing Reset Values");
-
 		matrix.printMatrix(reset);
 	}
 
@@ -63,117 +47,77 @@ public class Matrix {
 	 *         row and column values to zero.
 	 * 
 	 *         If you set matrix's corresponding row and column value to zero as
-	 *         you spot it the entire matrix will turn into zeros. Instead save
+	 *         you spot it the entire matrix will turn into zeros. Instead, save
 	 *         row and column values in a set and later loop through the row and
 	 *         column sets and set the row and columns to zero.
 	 */
 	public int[][] resetValues(int[][] matrix) {
-
 		if (matrix == null) {
-
 			return matrix;
 		}
-
 		Set<Integer> rows = new HashSet<Integer>();
-
 		Set<Integer> cols = new HashSet<Integer>();
-
 		for (int i = 0; i < matrix.length; i++) {
-
 			for (int j = 0; j < matrix.length; j++) {
-
 				if (matrix[i][j] == 0) {
-
 					rows.add(i);
-
 					cols.add(j);
 				}
 			}
-
 			for (int row : rows) {
-
 				for (int j = 0; j < matrix[0].length; j++) {
-
 					matrix[row][j] = 0;
 				}
 			}
-
 			for (int col : cols) {
-
 				for (int j = 0; j < matrix.length; j++) {
-
 					matrix[j][col] = 0;
 				}
 			}
 		}
-
 		return matrix;
 	}
 
 	public void printMainDiagonal(int array[][]) {
-
 		if (array != null) {
-
 			for (int i = 0; i < array.length; i++) {
-
 				System.out.print(array[i][i] + "\t");
 			}
-
 		}
 	}
-
 	public void printSecondDiagonal(int array[][]) {
-
 		if (array != null) {
-
 			int n = array.length - 1;
-
 			for (int i = 0; i <= n; i++) {
-
 				System.out.print(array[n - i][i] + "\t");
 			}
 		}
 	}
-
 	// If rows and columns are sorted.
-
 	public boolean findElement(int[][] matrix, int ele, int m, int n) {
-
 		int row = 0;
 		int col = n - 1;
-
 		while (row < m && col >= 0) {
-
 			if (matrix[row][col] == ele) {
-
 				return true;
 			} else if (matrix[row][col] > ele) {
-
 				col--;
 			} else {
 				row++;
 			}
-
 		}
-
 		return false;
 	}
 
 	public int findMax(int[][] matrix) {
-
 		int maxSoFar = Integer.MIN_VALUE;
-
 		for (int i = 0; i < matrix.length; i++) {
-
 			for (int j = 0; j < matrix[i].length; j++) {
-
 				if (matrix[i][j] > maxSoFar) {
-
 					maxSoFar = matrix[i][j];
 				}
 			}
 		}
-
 		return maxSoFar;
 	}
 
@@ -184,15 +128,10 @@ public class Matrix {
 	 * @param a
 	 */
 	public void printSpiralOrder(int a[][]) {
-
 		int m = a.length, n = a[0].length;
-
 		int rowStart = 0, rowEnd = m - 1, colStart = 0, colEnd = n - 1;
-
 		while (rowStart <= rowEnd && colStart <= colEnd) {
-
 			int i = rowStart, j = colStart;
-
 			for (j = colStart; j <= colEnd; j++)
 				System.out.print(a[i][j] + " ");
 			for (i = rowStart + 1, j--; i <= rowEnd; i++)
@@ -209,11 +148,8 @@ public class Matrix {
 	}
 
 	public void printMatrix(int[][] array) {
-
 		for (int i = 0; i < array.length; i++) {
-
 			for (int j = 0; j < array[0].length; j++) {
-
 				System.out.print(array[i][j] + " ");
 			}
 			System.out.println();
--- src/practice/SinglyLinkedListOps.java
+++ src/practice/SinglyLinkedListOps.java
@@ -1,5 +1,7 @@
 package practice;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 
 class List {
@@ -21,15 +23,11 @@ class List {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
         List other = (List) obj;
-        if (data != other.data)
-            return false;
+        if (data != other.data) return false;
         if (next == null) {
             return other.next == null;
         } else return next.equals(other.next);
@@ -65,8 +63,7 @@ public class SinglyLinkedListOps {
         System.out.println("Nth Node from end is: " + nThNode.data);
 
         List nThNodeFromBeginning = opsObj.nThFromBeginning(head, 6);
-        System.out.println("Nth Node from the beginning is: "
-                + nThNodeFromBeginning.data);
+        System.out.println("Nth Node from the beginning is: " + nThNodeFromBeginning.data);
         // head = opsObj.reverse(head);
         opsObj.print(head);
 
@@ -83,10 +80,11 @@ public class SinglyLinkedListOps {
         head = opsObj.addNodeBack(head, 256);
         head = opsObj.addNodeBack(head, 2048);
         // Remove duplicate nodes.
-        System.out.println("List with duplicates");
+        System.out.println("List with duplicates...");
         opsObj.print(head);
-        removeDuplicates(head);
-        System.out.println("List with unique elements");
+        // removeDuplicates(head);
+        removeDuplicatesEfficient(head);
+        System.out.println("List with unique elements after removal.....");
         opsObj.print(head);
 
         // Rearrange odd and even nodes.
@@ -113,14 +111,84 @@ public class SinglyLinkedListOps {
         deleteMiddleNode = opsObj.addNodeBack(deleteMiddleNode, 1);
         deleteMiddleNode = opsObj.addNodeBack(deleteMiddleNode, 2);
         deleteMiddleNode = opsObj.addNodeBack(deleteMiddleNode, 6);
-        /* deleteMiddleNode = deleteMiddleNode(deleteMiddleNode);
-        opsObj.print(deleteMiddleNode); */
+        deleteMiddleNode = deleteMiddleNode(deleteMiddleNode);
+        opsObj.print(deleteMiddleNode);
 
         System.out.println("Delete middle node of the list with only two nodes. ");
         deleteMiddleNode = new List(2);
         deleteMiddleNode = opsObj.addNodeBack(deleteMiddleNode, 1);
         deleteMiddleNode = deleteMiddleNode(deleteMiddleNode);
         opsObj.print(deleteMiddleNode);
+
+        // Find if there is any inersecting nodes. If there is return.
+
+        List n1 = new List(1);
+        n1.next = new List(2);
+        n1.next.next = new List(3);
+        n1.next.next.next = new List(4);
+        n1.next.next.next.next = new List(5);
+        n1.next.next.next.next.next = new List(6);
+        n1.next.next.next.next.next.next = new List(7);
+        // list 2
+        List n2 = new List(10);
+        n2.next = new List(9);
+        n2.next.next = new List(8);
+        n2.next.next.next = n1.next.next.next;
+        opsObj.print(n1);
+
+        opsObj.print(n2);
+        System.out.println("Has common node: " + opsObj.intersection(n1, n2).data);
+
+        // Partition the linked list.
+
+        List linkedList = null;
+        linkedList = opsObj.addNodeBack(linkedList, 10);
+        linkedList = opsObj.addNodeBack(linkedList, 4);
+        linkedList = opsObj.addNodeBack(linkedList, 8);
+        linkedList = opsObj.addNodeBack(linkedList, 16);
+        linkedList = opsObj.addNodeBack(linkedList, 20);
+        linkedList = opsObj.addNodeBack(linkedList, 2);
+        linkedList = opsObj.addNodeBack(linkedList, 3);
+        linkedList = opsObj.addNodeBack(linkedList, 7);
+        linkedList = opsObj.addNodeBack(linkedList, 11);
+        linkedList = opsObj.addNodeBack(linkedList, 9);
+        linkedList = opsObj.addNodeBack(linkedList, 1);
+        System.out.println("Original List: ");
+        opsObj.print(linkedList);
+        int pivot = 16;
+        linkedList = opsObj.partition(linkedList, pivot);
+        System.out.println("After partitioning around " + pivot + "\n");
+        opsObj.print(linkedList);
+
+        List palindrome = null;
+        palindrome = opsObj.addNodeBack(palindrome, 1);
+        palindrome = opsObj.addNodeBack(palindrome, 2);
+        palindrome = opsObj.addNodeBack(palindrome, 3);
+        palindrome = opsObj.addNodeBack(palindrome, 4);
+        palindrome = opsObj.addNodeBack(palindrome, 5);
+        palindrome = opsObj.addNodeBack(palindrome, 4);
+        palindrome = opsObj.addNodeBack(palindrome, 3);
+        palindrome = opsObj.addNodeBack(palindrome, 2);
+        palindrome = opsObj.addNodeBack(palindrome, 1);
+
+        System.out.println("Original Palindrome List: ");
+        opsObj.print(palindrome);
+        System.out.println("Is list a palindrome: " + isPalindrome(palindrome));
+
+        // Linked List with Cycle
+
+        List cycle = new List(50);
+        cycle.next = new List(20);
+        cycle.next.next = new List(15);
+        cycle.next.next.next = new List(4);
+        cycle.next.next.next.next = new List(10);
+
+        // Create a loop
+        cycle.next.next.next.next.next = cycle.next.next;
+
+        List collisionNode = opsObj.collision(cycle);
+        String loop = collisionNode == null ? "None" : String.valueOf(collisionNode.data);
+        System.out.println("Cycle starts at: " + loop);
 
         System.out.println("Remove given node. The node is not the head or the tail of the linked list");
     }
@@ -138,6 +206,17 @@ public class SinglyLinkedListOps {
         }
     }
 
+    /**
+     * following implementation uses two pointer technique and no extra space.
+     * As a result the space complexity of this algorithm is o(1)(constant space.)
+     * However the time complexity is o(N2) (big-o of N squared.)
+     * <p>
+     * To improve time complexity to O(N) we can use a Set. that is a tradeoff
+     * to achieve faster executing time. With additional space, the algorithm will
+     * be O(N) time complexity and O(N) space complexity.
+     *
+     * @param head
+     */
     public static void removeDuplicates(List head) {
         if (head != null && head.next != null) {
             List current = head;
@@ -149,6 +228,23 @@ public class SinglyLinkedListOps {
                     } else {
                         runner = runner.next;
                     }
+                }
+                current = current.next;
+            }
+        }
+    }
+
+    public static void removeDuplicatesEfficient(List head) {
+        Set<Integer> nodes = new HashSet<>();
+        if (head != null && head.next != null) {
+            List current = head;
+            List previous = null;
+            while (current != null) {
+                if (nodes.contains(current.data)) {
+                    previous.next = current.next;
+                } else {
+                    nodes.add(current.data);
+                    previous = current;
                 }
                 current = current.next;
             }
@@ -177,7 +273,7 @@ public class SinglyLinkedListOps {
         List curr = head;
         List fast = head;
 
-        if(head == null || head.next != null) {
+        if (head == null || head.next != null) {
             return null;
         }
         while (fast != null && fast.next != null) {
@@ -185,8 +281,8 @@ public class SinglyLinkedListOps {
             fast = fast.next.next;
         }
         // At this point current is at the middle of the list
-        if(curr != null) {
-            if(curr.next == null) {
+        if (curr != null) {
+            if (curr.next == null) {
                 curr = null;
                 head.next = curr;
             } else {
@@ -195,6 +291,24 @@ public class SinglyLinkedListOps {
             }
         }
         return head;
+    }
+
+    public static boolean isPalindrome(List l1) {
+        if (l1 == null) {
+            return false;
+        }
+        List current = l1;
+        Set<Integer> nodes = new HashSet<>();
+        while (current != null) {
+            int data = current.data;
+            if (nodes.contains(data)) {
+                nodes.remove(data);
+            } else {
+                nodes.add(data);
+            }
+            current = current.next;
+        }
+        return nodes.size() <= 1;
     }
 
     public List addNodeBack(List head, int data) {
@@ -330,10 +444,8 @@ public class SinglyLinkedListOps {
         List slow = first, fast = first; // create two references.
         while (true) {
             slow = slow.next; // 1 hop.
-            if (fast.next != null)
-                fast = fast.next.next; // 2 hops.
-            else
-                return false; // next node null => no loop.
+            if (fast.next != null) fast = fast.next.next; // 2 hops.
+            else return false; // next node null => no loop.
             if (slow == null || fast == null) // if either hits null..no loop.
                 return false;
             if (slow == fast) // if the two ever meet...we must have a loop.
@@ -361,22 +473,15 @@ public class SinglyLinkedListOps {
     }
 
     public List nThFromBeginning(List head, int n) {
-
         int count = 1;
-
         List pointer = head;
-
         while (count < n) {
-
             if (pointer == null) {
-
                 return null;
             }
             pointer = pointer.next;
-
             count++;
         }
-
         return pointer;
     }
 
@@ -390,42 +495,30 @@ public class SinglyLinkedListOps {
      */
 
     public List swapKthNode(List head, int k) {
-
         if (head == null) {
-
             return null;
         }
 
         List kthFromStart = head;
         List kthFromEnd = head;
-
         int count = 1;
-
         while (count < k) {
-
             if (kthFromStart == null) {
-
                 return null;
             }
             kthFromStart = kthFromStart.next;
             count++;
         }
-
-        List temp = kthFromStart;
-
+        List temp = kthFromStart; // reference to Kth node from start.
         while (temp != null) {
-
             temp = temp.next;
             kthFromEnd = kthFromEnd.next;
         }
-
         if (kthFromStart != null && kthFromEnd != null) {
-
             int buffer = kthFromStart.data;
             kthFromStart.data = kthFromEnd.data;
             kthFromEnd.data = buffer;
         }
-
         return head;
     }
 
@@ -438,44 +531,31 @@ public class SinglyLinkedListOps {
      * nodes. If they are equal then return the node else return null.
      */
     public List hasCommonNode(List l1, List l2) {
-
         if (l1 == null || l2 == null) {
-
             return null;
         }
-
         List head1 = l1;
         List head2 = l2;
-
         Stack<List> stack1 = new Stack<List>();
-
         Stack<List> stack2 = new Stack<List>();
 
         while (head1 != null) {
-
             stack1.push(head1);
-
             head1 = head1.next;
         }
 
         while (head2 != null) {
-
             stack2.push(head2);
-
             head2 = head2.next;
         }
 
         while (!stack1.isEmpty() || !stack2.isEmpty()) {
-
             List node1 = stack1.pop();
             List node2 = stack2.pop();
-
             if (node1.equals(node2)) {
-
                 return node1;
             }
         }
-
         return null;
     }
 
@@ -489,22 +569,18 @@ public class SinglyLinkedListOps {
     public boolean commonNode(List l1, List l2) {
 
         if (l1 == null || l2 == null) {
-
             return false;
         }
         List head1 = l1;
         List head2 = l2;
 
         while (head1.next != null) {
-
             head1 = head1.next;
         }
 
         while (head2.next != null) {
-
             head2 = head2.next;
         }
-
         return head1 == head2;
     }
 
@@ -515,66 +591,121 @@ public class SinglyLinkedListOps {
      * is any common node between them else return null.
      */
     public List mergeNode(List head1, List head2) {
-
         if (head1 == null || head2 == null) {
-
             return null;
         }
-
         int len1 = 0;
-
         int len2 = 0;
-
         List list1 = head1;
         List list2 = head2;
-
         while (list1 != null) {
-
             list1 = list1.next;
-
             len1++;
         }
         while (list2 != null) {
-
             list2 = list2.next;
-
             len2++;
         }
-
         list1 = head1;
         list2 = head2;
-
         int diff = Math.abs(len1 - len2);
-
         int count = 0;
-
         if (len1 > len2) {
-
             while (count < diff) {
-
                 list1 = list1.next;
                 count++;
             }
         } else if (len1 < len2) {
-
             while (count < diff) {
-
                 list2 = list2.next;
-
                 count++;
             }
         }
-
         while (list1 != null || list2 != null) {
-
             if (list1 == list2) {
-
                 return list1;
             }
-
             list1 = list1.next;
             list2 = list2.next;
         }
         return null;
+    }
+
+    /**
+     * This will not work if list sizes are not same.
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public List intersection(List l1, List l2) {
+        if (l1 == null || l2 == null) {
+            return null;
+        }
+        while (l1 != null && l2 != null) {
+            if (l1 == l2) {
+                return l1;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        return l1;
+    }
+
+    public List partition(List node, int pivot) {
+        if (node == null) {
+            return null;
+        }
+        List head = node;
+        List tail = node;
+
+        while (node != null) {
+            // Preserve current next
+            List next = node.next;
+            if (node.data < pivot) {
+                node.next = head;
+                head = node;
+            } else {
+                tail.next = node;
+                tail = node;
+            }
+            node = next;
+        }
+        // Remember to set tail.next to null otherwise you will get stack overflow.
+        tail.next = null;
+        return head;
+    }
+
+    public List collision(List head) {
+        List slow = head, fast = head;
+        boolean loopDetected = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                loopDetected = true;
+                break;
+            }
+        }
+        // if fast is now or next of fast is null that indicates
+        // there are no cycles in the linked list.
+       /* if (fast == null || fast.next == null) {
+            return null;
+        } */
+        // Now we are at a meeting point of fast and slow. Since slow has moved
+        // k nodes and fast is at the same position, we will move slow to the
+        // start of the linked list and have them move at the same speed this time.
+        // And have slow and fast move again. At some point they both will be again
+        // at the meeting point since now we know there is a loop in the linked list.
+
+        if(loopDetected) {
+            slow = head;
+            while (slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+        }
+        // Now fast and slow both are at the node from where the cycle starts.
+        return fast;
     }
 }
