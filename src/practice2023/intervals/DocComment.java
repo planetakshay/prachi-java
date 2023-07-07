da2076ae08cb20a37f9de62184aa98b667710b11
prachideodhar <prachi.deodhar@gmail.com>
2023-07-07 12:41:23 -0700

    new algorithm

--- src/practice2023/intervals/DocComment.java
+++ src/practice2023/intervals/DocComment.java
@@ -1,7 +1,6 @@
 package practice2023.intervals;
 
 import java.util.*;
-
 import static practice2023.intervals.IntervalIsCovered.getRange;
 
 /**
@@ -12,77 +11,108 @@ import static practice2023.intervals.IntervalIsCovered.getRange;
 public class DocComment {
 
     public static void main(String[] args) {
-        List<String> intervals = Arrays.asList("(0, 3): A", "(2, 4): B", "(5, 6): C");
-        List<String> output = mergeIntervalsWithLabels(intervals);
-        output.stream().forEach(st -> System.out.println(st));
+        System.out.println("\nInput: \n" + "(0, 3): A\n" + "(2, 4): B\n" + "(5, 6): C");
+        List<Interval> intervals = Arrays.asList(new Interval("(0, 3): A"), new Interval("(2, 4): B"), new Interval("(5, 6): C"));
+        List<Interval> output = mergeIntervalsWithLabels(intervals);
+        System.out.println("\nOutput: ");
+        output.stream().forEach(interval -> System.out.println(interval));
 
-        System.out.println("\n\n\n");
-        intervals = Arrays.asList("(0, 3): A", "(0, 3): B", "(2, 4): C", "(5, 6): D");
+        System.out.println("\n\nInput: \n(0, 3): A\n" + "(0, 3): B\n" + "(2, 4): C\n" + "(5, 6): D");
+        intervals = Arrays.asList(new Interval("(0, 3): A"), new Interval("(0, 3): B"), new Interval("(2, 4): C"), new Interval("(5, 6): D"));
         output = mergeIntervalsWithLabels(intervals);
-        output.stream().forEach(st -> System.out.println(st));
+        System.out.println("\nOutput: ");
+        output.stream().forEach(interval -> System.out.println(interval));
     }
 
-    public static List<String> mergeIntervalsWithLabels(List<String> intervals) {
-
+    public static List<Interval> mergeIntervalsWithLabels(List<Interval> intervals) {
         if (intervals == null || intervals.isEmpty()) {
             return null;
         }
-        List<String> splitIntervals = new LinkedList<>();
-        String firstInterval = intervals.get(0);
-        String[] first = firstInterval.split(":");
-        int[] current = getRange(first[0]);
-        String currLabel = getLabel(intervals.get(0));
-
         int size = intervals.size();
-        for (int i = 0; i < size; i++) {
-            String interval = intervals.get(i);
-            String[] temp = interval.split(":");
-            int[] next = getRange(temp[0]);
-            String nextLabel = getLabel(interval);
+        Interval curr = intervals.get(0);
+        List<Interval> splitIntervals = new LinkedList<>();
+        for (int i = 1; i < size; i++) {
 
-            int currBegin = current[0];
-            int currEnd = current[1];
-            int nextBegin = next[0];
-            int nextEnd = next[1];
+            Interval next = intervals.get(i);
 
-            String newInterval = "";
-
-            if ((currBegin == nextBegin && currEnd == nextEnd) && currLabel.equalsIgnoreCase(nextLabel)) {
+            if (curr.equals(next)) {
                 continue;
-            } else if ((currBegin == nextBegin && currEnd == nextEnd) && !currLabel.equalsIgnoreCase(nextLabel)) {
-                splitIntervals.add(buildNewInterval(current, currLabel, nextLabel));
-            } else if (currEnd > nextBegin) {
-                int[] newInt = new int[2];
-                newInt[0] = currBegin;
-                newInt[1] = nextBegin;
-                splitIntervals.add(buildNewInterval(newInt, currLabel, ""));
+            } else if (curr.getStart() == next.getStart() && curr.getEnd() == next.getEnd() && !curr.getLabel().equals(next.getLabel())) {
+                // merge only label.
+                List<String> labelsSoFar = new LinkedList<>(curr.getLabel());
+                labelsSoFar.addAll(next.getLabel());
+                curr.setLabel(labelsSoFar);
 
-                newInt = new int[2];
-                newInt[0] = nextBegin;
-                newInt[1] = currEnd;
-                splitIntervals.add(buildNewInterval(newInt, currLabel, nextLabel));
+            } else if (curr.getEnd() > next.getStart()) {
+                //merge intervals and labels.
+                List<String> mergedLabels = new LinkedList<>();
+                mergedLabels.addAll(curr.getLabel());
+                mergedLabels.addAll(next.getLabel());
 
-                newInt = new int[2];
-                newInt[0] = currEnd;
-                newInt[1] = nextEnd;
-                splitIntervals.add(buildNewInterval(newInt, "", nextLabel));
+                Interval split = new Interval(curr.getStart(), next.getStart(), curr.getLabel());
+                splitIntervals.add(split);
 
+                split = new Interval(next.getStart(), curr.getEnd(), mergedLabels);
+                splitIntervals.add(split);
+
+                split = new Interval(curr.getEnd(), next.getEnd(), next.getLabel());
+                splitIntervals.add(split);
             } else {
-                splitIntervals.add(buildNewInterval(next, "", nextLabel));
+                splitIntervals.add(next);
             }
         }
         return splitIntervals;
     }
+}
 
-    public static String buildNewInterval(int[] curr, String currLabel, String nextLabel) {
-        return "(" + curr[0] + "," + curr[1] + "):" + "[" + currLabel + ", " + nextLabel + "]";
+class Interval {
+    int start;
+    int end;
+    List<String> label;
+    public Interval(int start, int end, List<String> labels) {
+        this.start = start;
+        this.end = end;
+        this.label = labels;
     }
-
-    public static String getLabel(String interval) {
+    public Interval(String interval) {
         String[] splits = interval.split(":");
-        if (splits.length > 1) {
-            return splits[1].trim();
-        }
-        return "";
+        int[] range = getRange(splits[0]);
+
+        this.start = range[0];
+        this.end = range[1];
+
+        this.label = List.of(splits[1].trim());
+    }
+    public int getStart() {
+        return start;
+    }
+    public void setStart(int start) {
+        this.start = start;
+    }
+    public int getEnd() {
+        return end;
+    }
+    public void setEnd(int end) {
+        this.end = end;
+    }
+    public List<String> getLabel() {
+        return label;
+    }
+    public void setLabel(List<String> label) {
+        this.label = label;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Interval interval)) return false;
+        return start == interval.start && end == interval.end && Objects.equals(label, interval.label);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end, label);
+    }
+    @Override
+    public String toString() {
+        return "(" + start + ", " + end + ") : " + Arrays.toString(label.toArray());
     }
 }
