04a81d6f1ffe19e27c0cf49682ae6b2f44af98c5
prachideodhar <prachi.deodhar@gmail.com>
2023-10-05 06:53:11 -0700

    new algorithm

--- src/practice2023/intervals/MeetingRoom.java
+++ src/practice2023/intervals/MeetingRoom.java
@@ -1,11 +1,32 @@
 package practice2023.intervals;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/meeting-rooms/
+ *
+ *
  */
 public class MeetingRoom {
+    public static void main(String[] args) {
+        int[][] intervals = new int[][]{{0,30},{5,10},{15,20}};
+        System.out.println("Can attend all the meetings: " + bruteForce(intervals));
 
-
+        intervals = new int[][]{{7,10},{2,4}};
+        System.out.println("Can attend all the meetings: " + bruteForce(intervals));
+    }
+    public static boolean bruteForce(int[][] intervals) {
+        if(intervals == null || intervals.length == 0) {
+            return true;
+        }
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        for(int i=0;i<intervals.length - 1; i++) {
+            if(intervals[i][1] > intervals[i+1][0]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 class Meeting {
