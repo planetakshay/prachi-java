3e3d99e2ce05f4a343426ddc58171e7ef64d0b68
prachideodhar <prachi.deodhar@gmail.com>
2023-11-13 20:22:42 -0800

    new algorithm

---
+++ src/practice2023/Dynamicprogramming.java
@@ -0,0 +1,47 @@
+package practice2023;
+
+/**
+ * Find all possible unique paths from (0,0) to (m-1, n-1)
+ * in a m x n matrix with obstacles.
+ *
+ * https://leetcode.com/problems/unique-paths-ii/
+ *
+ * covered in IK timed test.
+ *
+ * Inteview Kickstart
+ */
+public class Dynamicprogramming {
+    public static void main(String args) {
+
+    }
+
+    public int findUniquePaths(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] memo = new int[m][n];
+        // 0 should be treated as an obstacle.
+        // We either cannot start the traversal or destination is the
+        // obstacle making it impossible to reach the last cell in the grid
+        if(grid[0][0] == 0 || grid[m-1][n-1] == 0) {
+            return 0;
+        }
+        memo[0][0] = 1;
+        for(int i=1;i<n;i++) {
+            if(grid[0][i] != 0) {
+                memo[0][i] = memo[0][i-1];
+            }
+        }
+        for(int i=1;i<m;i++) {
+            if(grid[i][0] != 0) {
+                memo[i][0] = memo[i-1][0];
+            }
+        }
+
+        for(int i=1;i < m;i++) {
+            for(int j = 1; j < n; j++) {
+                memo[i][j] = memo[i-1][j] + memo[i][j-1];
+            }
+        }
+        return memo[m-1][n-1];
+    }
+}
--- src/practice2023/graph/TransposeGraph.java
+++ src/practice2023/graph/TransposeGraph.java
@@ -1,8 +1,6 @@
 package practice2023.graph;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Covered in IK timed tests for Graphs
@@ -15,17 +13,44 @@ public class TransposeGraph {
         map = new HashMap<>();
         return dfs(node);
     }
+
     public GraphNode dfs(GraphNode node) {
-        if(map.containsKey(node)) {
+        if (map.containsKey(node)) {
             return map.get(node);
         }
         map.put(node, new GraphNode(node.value));
-        for(GraphNode nei : node.neighbors) {
+        for (GraphNode nei : node.neighbors) {
             GraphNode newNei = dfs(nei);
             GraphNode newSrc = map.get(node);
             newNei.neighbors.add(newSrc);
         }
         return map.get(node);
+    }
+
+    public GraphNode bfs(GraphNode src) {
+        Set<GraphNode> visited = new HashSet<>();
+        Queue<GraphNode> queue = new LinkedList<>();
+        visited.add(src);
+        queue.add(src);
+        while (!queue.isEmpty()) {
+            GraphNode node = queue.poll();
+            if (!map.containsKey(node)) {
+                GraphNode newNode = new GraphNode(node.value);
+                map.put(node, newNode);
+            }
+            for (GraphNode nei : node.neighbors) {
+                if (!visited.contains(nei)) {
+                    if (map.containsKey(nei)) {
+                        GraphNode newNei = new GraphNode(nei.value);
+                        map.put(nei, newNei);
+                    }
+                    visited.add(nei);
+                    queue.offer(nei);
+                }
+                map.get(nei).neighbors.add(map.get(node));
+            }
+        }
+        return map.get(src);
     }
 }
 
