b2afd80de91658c760c35055be96651cd48a1356
prachideodhar <prachi.deodhar@gmail.com>
2024-01-30 20:10:17 -0800

    new algorithm

--- src/practice2023/tree/NextNodeInTree.java
+++ src/practice2023/tree/NextNodeInTree.java
@@ -10,20 +10,20 @@ public class NextNodeInTree {
     Map<Integer, Tree> map;
 
     public static void main(String[] args) {
-        NextNodeInTree sol = new NextNodeInTree();
-        Tree root = sol.createTree();
-        sol.root = root;
-        sol.populateMap(root);
-        System.out.println("Nodes to the right: " + sol.getNodesToTheRight(2));
-        System.out.println("Nodes to the right: " + sol.getNodesToTheRight(5));
+        NextNodeInTree nextNodeInTree = new NextNodeInTree();
+        Tree root = nextNodeInTree.createTree();
+        nextNodeInTree.root = root;
+        nextNodeInTree.populateMap(root);
+        System.out.println("Nodes to the right: " + nextNodeInTree.getNodesToTheRight(2));
+        System.out.println("Nodes to the right: " + nextNodeInTree.getNodesToTheRight(5));
     }
 
     public List<Integer> getNodesToTheRight(int target) {
         Tree node = map.get(target);
         List<Integer> result = new ArrayList<>();
-        while(node != null) {
+        while (node != null) {
             node = node.next;
-            if(node != null) {
+            if (node != null) {
                 result.add(node.value);
             }
         }
@@ -31,7 +31,7 @@ public class NextNodeInTree {
     }
 
     public void populateMap(Tree root) {
-        if(root != null) {
+        if (root != null) {
             bfs(root);
         }
     }
@@ -40,22 +40,22 @@ public class NextNodeInTree {
         map = new HashMap<>();
         Queue<Tree> queue = new LinkedList<>();
         queue.add(node);
-        while(!queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             int size = queue.size();
-            for(int i=0; i < size; i++) {
+            for (int i = 0; i < size; i++) {
                 Tree curr = queue.poll();
                 // The if condition below is very important
                 // without which the last node in the current level
                 // will have its next node pointing to the first node
                 // of the next level.
-                if(i < size - 1) {
+                if (i < size - 1) {
                     curr.next = queue.peek();
                 }
                 map.put(curr.value, curr);
-                if(curr.left != null) {
+                if (curr.left != null) {
                     queue.offer(curr.left);
                 }
-                if(curr.right != null) {
+                if (curr.right != null) {
                     queue.offer(curr.right);
                 }
             }
@@ -63,31 +63,21 @@ public class NextNodeInTree {
     }
 
     public Tree createTree() {
-        Tree node1 = new Tree();
-        node1.value = 2;
+        Tree node1 = new Tree(2);
+        Tree node2 = new Tree(3);
 
-        Tree node2 = new Tree();
-        node2.value = 3;
+        Tree root = new Tree(1, node1, node2);
 
-        Tree root = new Tree();
-        root.value = 1;
-        root.left = node1;
-        root.right = node2;
-
-        Tree node4 = new Tree();
-        node4.value = 4;
+        Tree node4 = new Tree(4);
         node1.left = node4;
 
-        Tree node5 = new Tree();
-        node5.value = 5;
+        Tree node5 = new Tree(5);
         node1.right = node5;
 
-        Tree node6 = new Tree();
-        node6.value = 6;
+        Tree node6 = new Tree(6);
         node2.left = node6;
 
-        Tree node7 = new Tree();
-        node7.value = 7;
+        Tree node7 = new Tree(7);
         node2.right = node7;
 
         return root;
@@ -100,7 +90,13 @@ class Tree {
     Tree right;
     Tree next;
 
-    public Tree() {
+    public Tree(int value) {
+        this.value = value;
+    }
 
+    public Tree(int value, Tree left, Tree right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
     }
 }
